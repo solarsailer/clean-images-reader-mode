2c4adb427e59ea994f6db9fd3ab3170e42749f6a
Matthieu Oger <m.oger@theexplorers.com>
2023-09-12 15:02:50 +0200

    Add script

--- clean-images-reader-mode.js
+++ clean-images-reader-mode.js
@@ -1,1 +1,16 @@
-javascript: (function () {})()
+javascript: (function () {
+  const removeList = ['loading', 'sizes', 'srcset', 'decoding']
+
+  function purgeImageTag(node) {
+    const attributes = Array.from(node.attributes)
+
+    attributes.forEach(attr => {
+      const name = attr.name
+      if (name.startsWith('data-') || removeList.includes(name)) {
+        node.removeAttribute(name)
+      }
+    })
+  }
+
+  document.querySelectorAll('img').forEach(purgeImageTag)
+})()
